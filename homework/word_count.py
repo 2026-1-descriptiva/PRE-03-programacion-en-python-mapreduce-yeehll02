14647fdf3d401bdfe6501df3c6c306785967e795
Yesica Andrea Henao Ceballos <116982357+yeehll02@users.noreply.github.com>
2026-02-27 18:58:36 -0500

    Refactor word count script with new functions

--- homework/word_count.py
+++ homework/word_count.py
@@ -1,82 +1,140 @@
 """Taller evaluable"""
 
-# pylint: disable=broad-exception-raised
-
-import fileinput
 import glob
-import os.path
+import os
+import shutil
+import string
 import time
-from itertools import groupby
-
-from toolz.itertoolz import concat, pluck
 
 
-
-def copy_raw_files_to_input_folder(n):
-    """Generate n copies of the raw files in the input folder"""
-
-
-
-def load_input(input_directory):
-    """Funcion load_input"""
-
-
-def preprocess_line(x):
-    """Preprocess the line x"""
+def prepate_input_dicrectory(path: str):
+    """Crea la carpeta si no existe."""
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
+    else:
+        # Elimina todos los archivos en la carpeta
+        for file in glob.glob(f"{path}/*"):
+            os.remove(file)
 
 
-def map_line(x):
-    pass
+# Copia los archivos raw a {input_path} usando python multi plataforma
+def copy_raw_files_to_input_folder(n, raw_path, input_path):
+    for file in glob.glob(f"{raw_path}/*"):
+        print(f"Copying {file} to {input_path} " f" {n} times")
+        with open(file, "r", encoding="utf-8") as f:
+            content = f.read()
+        # Crea NUMERO_COPIAS copias del archivo
+        for i in range(n):
+            raw_file_name = os.path.basename(file)
+            raw_file_name_without_ext, raw_file_ext = os.path.splitext(raw_file_name)
+            with open(
+                f"{input_path}/{raw_file_name_without_ext}_{i}.{raw_file_ext.lstrip('.')}",
+                "w",
+                encoding="utf-8",
+            ) as f:
+                f.write(content)
+
 
 def mapper(sequence):
-    """Mapper"""
+    pairs_sequence = []
+    for _, line in sequence:
+        line = line.lower()
+        line = line.translate(str.maketrans("", "", string.punctuation))
+        line = line.replace("\n", "")
+        words = line.split()
+        pairs_sequence.extend([(word, 1) for word in words])
+    return pairs_sequence
 
 
-def shuffle_and_sort(sequence):
-    """Shuffle and Sort"""
+def reducer(pairs_sequence):
+    result = []
+    for key, value in pairs_sequence:
+        if result and result[-1][0] == key:
+            result[-1] = (key, result[-1][1] + value)
+        else:
+            result.append((key, value))
+    return result
 
 
+def hadoop(mapper_func, reducer_func, input_path, output_path):
+    """Simula el comportamiento de Hadoop Streaming."""
 
-def compute_sum_by_group(group):
-    pass
+    def emit_input_lines(input_path):
+        sequence = []
+        files = glob.glob(f"{input_path}/*")
+        for file in files:
+            with open(file, "r", encoding="utf-8") as f:
+                for line in f:
+                    sequence.append((file, line))
+        return sequence
 
-def reducer(sequence):
-    """Reducer"""
+    def create_output_folder(output_dir):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        else:
+            raise FileExistsError(f"The folder {output_dir} already exists")
+
+    def shuffle_and_sort(pairs_sequence):
+        pairs_sequence = sorted(pairs_sequence)
+        return pairs_sequence
+
+    def export_results_to_file(output_path, result):
+        with open(f"{output_path}/part-00000", "w", encoding="utf-8") as f:
+            for key, value in result:
+                f.write(f"{key}\t{value}\n")
+
+    def create_success_file(output_path):
+        """Create Marker"""
+        with open(f"{output_path}/_SUCCESS", "w", encoding="utf-8") as f:
+            f.write("")  # archivo vacío
+
+    # Lee los archivos de {input_path}
+    sequence = emit_input_lines(input_path)
+
+    # Mapea las líneas a pares (palabra, 1). Este es el mapper.
+    pairs_sequence = mapper_func(sequence)
+
+    # ordena la secuencia de pares por palabra
+    pairs_sequence = shuffle_and_sort(pairs_sequence)
+
+    # Reduce la secuencia de pares sumando los valores para cada palabra. este es el reducer.
+    result = reducer_func(pairs_sequence)
+
+    # Crea la carpeta {output_path}
+    create_output_folder(output_path)
+
+    # Guarda el resultado en un archivo {output_path}/part-00000
+    export_results_to_file(output_path, result)
+
+    # crea un archivo _SUCCESS en {output_path}
+    create_success_file(output_path)
 
 
-def create_directory(directory):
-    """Create Output Directory"""
+def run_job(n: int = 1000):
+    """Ejecuta el job de word count."""
+    raw_path = "files/raw"
+    input_path = "files/input"
+    output_path = "files/output"
 
+    prepate_input_dicrectory(input_path)
+    copy_raw_files_to_input_folder(n, raw_path, input_path)
 
-def save_output(output_directory, sequence):
-    """Save Output"""
+    # El experimento realmente empieza en este punto.
+    start_time = time.time()
 
+    # Elimina la carpeta output/ si existe
+    if os.path.exists("files/output/"):
+        for file in os.listdir("files/output/"):
+            file_path = os.path.join("files/output/", file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        os.rmdir("files/output/")
 
-def create_marker(output_directory):
-    """Create Marker"""
+    hadoop(mapper, reducer, input_path, output_path)
 
-
-def run_job(input_directory, output_directory):
-    """Job"""
-    sequence = load_input(input_directory)
-    sequence = mapper(sequence)
-    sequence = shuffle_and_sort(sequence)
-    sequence = reducer(sequence)
-    create_directory(output_directory)
-    save_output(output_directory, sequence)
-    create_marker(output_directory)
+    # Imprime el tiempo que tomó el experimento
+    print(f"Tiempo de ejecución: {time.time() - start_time} segundos")
 
 
 if __name__ == "__main__":
-
-    copy_raw_files_to_input_folder(n=1000)
-
-    start_time = time.time()
-
-    run_job(
-        "files/input",
-        "files/output",
-    )
-
-    end_time = time.time()
-    print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
+    run_job(n=10000)
